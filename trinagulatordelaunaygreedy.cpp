efcb18d9f49251ef4d6ad8a4a8d4385ad67cc76e
NeoPhix <petr-ermolaev@hotmail.com>
2018-01-14 17:44:59 +0300

    progress

--- trinagulatordelaunaygreedy.cpp
+++ trinagulatordelaunaygreedy.cpp
@@ -15,5 +15,39 @@ std::list<Triangle> TrinagulatorDelaunayGreedy::triangulatePointCloud(std::vecto
 
     std::list<Triangle> result;
 
-    return std::list<Triangle>();
+    result.push_back( Triangle{ points[0], points[1], points[2] } );    //the first triangle is built from three first points
+
+    for (size_t i = trianglePointsCount; i < points.size(); ++i)
+    {
+        addPointToTriangulation(result, points[i]);
+    }
+
+    return result;
+}
+
+void TrinagulatorDelaunayGreedy::addPointToTriangulation(std::list<Triangle> &triangulation, const Point3D &point)
+{
+    //TODO realize
+}
+
+TrinagulatorDelaunayGreedy::Position TrinagulatorDelaunayGreedy::insideTriangle(const Point3D &p, const Triangle &t)
+{
+    int ABProduct = (t.A - p) ^ (t.B - p);
+    int BCProduct = (t.B - p) ^ (t.C - p);
+    int CAProduct = (t.C - p) ^ (t.A - p);
+
+    bool allNegative = (ABProduct < 0) && (BCProduct < 0) && (CAProduct < 0);
+    bool allPositive = (ABProduct > 0) && (BCProduct > 0) && (CAProduct > 0);
+
+    if (allNegative || allPositive)
+        return Position::Inside;
+
+    if (ABProduct == 0)
+        return Position::OnEdgeAB;
+    else if (BCProduct == 0)
+        return Position::OnEdgeBC;
+    else if (CAProduct == 0)
+        return Position::OnEdgeCA;
+
+    return Position::Outside;
 }
--- trinagulatordelaunaygreedy.h
+++ trinagulatordelaunaygreedy.h
@@ -12,6 +12,18 @@ public:
     virtual std::list<Triangle> triangulatePointCloud(std::vector<Point3D> points) override;
 
 private:
+    enum class Position
+    {
+        Inside,
+        OnEdgeAB,
+        OnEdgeBC,
+        OnEdgeCA,
+        Outside
+    };
+
+private:
+    void addPointToTriangulation(std::list<Triangle> &triangulation, const Point3D &point);
+    Position insideTriangle(const Point3D &point, const Triangle &triangle);
 
 };
 
