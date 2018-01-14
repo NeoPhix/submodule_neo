10001e3a16a410fddc8ffd02d2b2b2cfad47c144
NeoPhix <petr-ermolaev@hotmail.com>
2018-01-14 15:49:14 +0300

    add condition of trinagle points count

--- trinagulatordelaunaygreedy.cpp
+++ trinagulatordelaunaygreedy.cpp
@@ -1,7 +1,17 @@
 #include "trinagulatordelaunaygreedy.h"
 #include "geometrystrutctures.h"
 
+static constexpr size_t trianglePointsCount = 3;
+
 std::list<Triangle> TrinagulatorDelaunayGreedy::triangulatePointCloud(std::list<Point3D> points)
 {
+    if (points.size() < trianglePointCount)
+    {
+        std::cout << "The count of points is not enough for triangulation" << std::endl;
+        return std::list<Triangle>();
+    }
+
+    std::list<Triangle> result;
+
     return std::list<Triangle>();
 }
